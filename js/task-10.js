940cc7f20855bf85af621d1d24a78465691b63af
Arkadiusz-Kruk <92742646+Arkadiusz-Kruk@users.noreply.github.com>
2022-06-30 11:31:27 +0200

    Upd 10

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,35 @@
+
+const inputNumber = document.querySelector("#controls input");
+const btnCreate = document.querySelector("[data-create]");
+const btnDestroy = document.querySelector("[data-destroy]");
+const boxes = document.querySelector("#boxes");
+
+btnCreate.addEventListener("click", createBoxes);
+btnDestroy.addEventListener("click", destroyBoxes);
+
+
+
+function createBoxes(amount) {
+  amount = inputNumber.value;
+  const box = [];
+  
+  for (let i = 0; i < amount; i++) {
+    let size = 30 + i * 10;
+    let color = getRandomHexColor();
+  
+    box.push(`<div style="height:${size}px ; width:${size}px ; background-color: ${color};"></div>`);
+  }
+  
+  const markup = box.join("");
+  return boxes.insertAdjacentHTML("afterbegin", markup);
+}
+function destroyBoxes() {
+ const boxAll = document.querySelectorAll('#boxes div');
+  boxAll.forEach(box => box.remove());
+  inputNumber.value = 0;
+};
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
+};
+
